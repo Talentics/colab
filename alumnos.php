10ea1accaf6669accb28261bc1822125ae5bcbc4
Alejandro <aleerdzmtz15@hotmail.com>
2017-05-31 17:35:18 -0500

    ya wey alumnos

--- alumnos.php
+++ alumnos.php
@@ -10,20 +10,20 @@
 <body>
 <header>
 		<ul id="menu">
-			<li class="item"><a href="inicio.html">Inicio</a></li>
-			<li class="item"><a href="alumnos.php">Alumnos</a></li>
+			<li class="item"><a href="../admin">Inicio</a></li>
+			<li class="item"><a href="../alumnos">Alumnos</a></li>
 			<li class="item"><a href="cursos.php">Cursos</a></li>
 			<li class="item"><a href="usuarios.php">Usuarios</a></li>
 			<li class="item"><a href="nosotros.php">Nosotros</a></li>
-			<li class="item"><a href="../colab/ayuda.php">Ayuda</a></li>
+			<li class="item"><a href="#Ayuda">Ayuda</a></li>
 			<li class="item"><a href="../../php/logout.php">Salir</a></li>
 
 		</ul>
 </header>
-
-<img src="../img/bienvenido.png" id="welcome">
-	
-</img>
-
+	<div class="containerUsuarios">
+		<h1>Usuarios</h1>
+		<p>Bienvenido a la página de Alumnos by Alex</p>
+		
+	</div>
 </body>
 </html>
